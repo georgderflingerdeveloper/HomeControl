e4e67e1fb1557adbd818fbd087011363290e785b
Georg Derflinger <georgderflinger@gmail.com>
2019-04-02 10:13:18 +0200

    start with sleepingroom

---
+++ HomeControl/ROOMS/SLEEPING_ROOM/SleepingRoomController.cs
@@ -0,0 +1,20 @@
+﻿using BASIC_COMPONENTS;
+using HomeControl.ADVANCED_COMPONENTS.Interfaces;
+using HomeControl.BASIC_COMPONENTS.Interfaces;
+using HomeControl.ADVANCED_COMPONENTS;
+using BASIC_CONTROL_LOGIC;
+using HomeControl.BASIC_CONSTANTS;
+using HomeControl.ROOMS.ANTE_BATH_WASH_ROOM_CONTROLLER.INTERFACE;
+using LibUdp.BASIC.INTERFACE;
+using LibUdp.BASIC.RECEIVE;
+using HomeAutomationProtocoll;
+using System;
+using SystemServices;
+using HardConfig;
+
+namespace HomeControl.ROOMS.SLEEPING_ROOM
+{
+    class SleepingRoomController : Controller
+    {
+    }
+}
