c46c83380f3d884b0f189bfc1717119fba604bec
Georg Derflinger <georgderflinger@gmail.com>
2019-04-25 21:23:46 +0200

    some restruct, kik out incompatible DLL - so that moq is working agina

--- HomeControl/ROOMS/SLEEPING_ROOM/SleepingRoomController.cs
+++ HomeControl/ROOMS/SLEEPING_ROOM/SleepingRoomController.cs
@@ -11,10 +11,48 @@ using HomeAutomationProtocoll;
 using System;
 using SystemServices;
 using HardConfig;
+using HomeControl.ROOMS.CONFIGURATION;
 
 namespace HomeControl.ROOMS.SLEEPING_ROOM
 {
     class SleepingRoomController : Controller
     {
+        #region DECLARATION
+        SleepingRoomConfiguration _config;
+        IIOHandler _IOHandler;
+        IUdpBasic _Communicator;
+        int _ScenarioNumber;
+
+        #endregion
+
+        public SleepingRoomController(SleepingRoomConfiguration config, IIOHandler IOHandler, IUdpBasic Communicator) : base()
+        {
+            _config = config;
+            _IOHandler = IOHandler;
+            _IOHandler.EDigitalInputChanged  += IOHandler_EDigitalInputChanged;
+            _IOHandler.EDigitalOutputChanged += IOHandler_EDigitalOutputChanged;
+            //Constructor();
+            _Communicator = Communicator;
+            _Communicator.EDataReceived += Communicator_EDataReceived;
+        }
+
+        private void Communicator_EDataReceived(object sender, DataReceivingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IOHandler_EDigitalInputChanged(object sender, DigitalInputEventargs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void IOHandler_EDigitalOutputChanged(object sender, DigitalOutputEventargs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region PROPERTIES
+        public int ScenarioNumber { get => _ScenarioNumber; set => _ScenarioNumber = value; }
+        #endregion
     }
 }
--- HomeControl/ROOMS/SLEEPING_ROOM/UNIT_TESTS/SleepingRoomController_UnitTests.cs
+++ HomeControl/ROOMS/SLEEPING_ROOM/UNIT_TESTS/SleepingRoomController_UnitTests.cs
@@ -7,11 +7,44 @@ using LibUdp;
 using LibUdp.BASIC.INTERFACE;
 using LibUdp.BASIC.RECEIVE;
 using HomeAutomationProtocoll;
+using HomeControl.ROOMS.CONFIGURATION;
+using System.Threading.Tasks;
 
 namespace HomeControl.ROOMS.SLEEPING_ROOM.UNIT_TESTS
 {
     [TestFixture]
     class SleepingRoomController_UnitTests
     {
+        static int AnyUnknownScenarioNumber = 999;
+        SleepingRoomController       _TestSleepingRoomController;
+        SleepingRoomConfiguration    _TestSleepingRoomConfiguration;
+        DigitalInputEventargs        _TestArgs = new DigitalInputEventargs();
+        Mock<IIOHandler>             _MockTestIOHandler;
+        Mock<IUdpBasic>              _MockUdpCommunicator;
+        Mock<SleepingRoomController> _MockSleepingRoomController;
+
+        public SleepingRoomController_UnitTests()
+        {
+            _MockUdpCommunicator = new Mock<IUdpBasic>();
+            _MockTestIOHandler   = new Mock<IIOHandler>();
+            _TestSleepingRoomConfiguration = new SleepingRoomConfiguration();
+            _TestSleepingRoomController = new SleepingRoomController(_TestSleepingRoomConfiguration, _MockTestIOHandler.Object, _MockUdpCommunicator.Object);
+        }
+
+        [Test]
+        public void Test_FirstScenario()
+        {
+            // prepare test
+            _TestSleepingRoomController.ScenarioNumber = AnyUnknownScenarioNumber;
+            _TestArgs.Index = IOAssignmentControllerAnteBathWashRoom.indDigitalInputBathRoomMainButton;
+            _TestArgs.Value = true;
+
+            // action
+            _MockTestIOHandler.Raise(DigitalInputChanged => DigitalInputChanged.EDigitalInputChanged += null, _TestArgs);
+
+            // proove
+            Assert.AreEqual(0, _TestSleepingRoomController.ScenarioNumber );
+        }
+
     }
 }
