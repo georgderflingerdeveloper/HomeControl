5ddec09ad6fc08973bdc384d1f62ec2db95906d6
GeorgDerflinger <georgderflinger@gmail.com>
2019-05-23 04:55:00 +0200

    spacing

--- HomeControl/HomeControlMain.cs
+++ HomeControl/HomeControlMain.cs
@@ -195,7 +195,8 @@ namespace HomeControl
                                                             new Timer_(0) 
                                                             );
 
-            IExtendedLightCommander ExtendedLightCommander = new ExtendedLightCommander(CommanderConfig,
+            IExtendedLightCommander ExtendedLightCommander = new ExtendedLightCommander(
+                                                                CommanderConfig,
                                                                 ControlTimer,
                                                                 ScenarioControl);
 
